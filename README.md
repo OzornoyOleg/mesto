0be3ea5460c8b5ab3aa8019e8d5ca3e85dbbba7b
Alexey <by.ryazan@gmail.com>
2022-11-26 19:18:48 +0300

    Added my work

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Практическая работа 4-го спринта
+# Практическая работа 5-го спринта
 
-Здесь находится практическая работа 4-го спринта, где мы можем редактировать профиль на главной странице.
+Здесь находится практическая работа 5-го спринта, где мы можем редактировать профиль на главной странице.
 
 Были применены основные свойства:
 
@@ -9,7 +9,9 @@
 3. Flexbox
 4. Технология JavaScript
 
+Также здесь применены новые знания JS, изученные в данном сприте, связанные с template-элементами, клонированием элементов, работой с массивами и прочим.
 
-Макет в Figma - https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=28212%3A212&t=SxwBwX9GyPosujVA-0
+Макет в Figma прошлого спринта - https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=28212%3A212&t=SxwBwX9GyPosujVA-0
+Макет в Figma данного спринта - https://www.figma.com/file/bjyvbKKJN2naO0ucURl2Z0/JavaScript.-Sprint-5?node-id=0%3A1&t=BydUWGmMXaN1MB41-0
 
 Страница моего проекта на Github Pages - https://ozornoyoleg.github.io/mesto/index.html
--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,160 @@
-let popup = document.querySelector('.popup')
-let popupInputName = popup.querySelector('.popup__input_type_name')
-let popupDescription = popup.querySelector('.popup__input_type_description')
-let popupClose = popup.querySelector('.popup__close')
+let popupAdd = document.querySelector('.popup_type_add')
+let popupInputName = document.querySelector('.popup__input_type_name')
+let popupDescription = document.querySelector('.popup__input_type_description')
+let popupClose = document.querySelectorAll('.popup__close')
 let profile = document.querySelector('.profile')
 let profileTitle = profile.querySelector('.profile__title')
 let profileSubtitle = profile.querySelector('.profile__subtitle')
 let profileEdit = profile.querySelector('.profile__edit-button')
-let formElement = document.querySelector('.popup__form')
+let addImage = profile.querySelector('.profile__add-button')
+let popupEdit = document.querySelector('.popup_type_edit')
+let formElementEdit = popupEdit.querySelector('.popup__form_type_edit')
+let formElementAdd = document.querySelector('.popup__form_type_add')
+let gridTemplate = document.querySelector('.grid-element').content.querySelector('.elements__element')
+let gridElements = document.querySelector('.elements')
+let popupCloseBt = popupAdd.querySelector('.popup__close')
+let popupImage = document.querySelector('.popup__image')
+let popupImageTitle = document.querySelector('.popup__image-title')
+let popupImageView = document.querySelector('.popup_type_image')
 
-function popupOpened() {
-  popup.classList.add('popup_active')
+let initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
+const popupEls = document.querySelectorAll('.popup')
+
+popupEls.forEach( function(popupEl) {
+  const popupCloseBtnEl = popupEl.querySelector('.popup__close');
+  popupCloseBtnEl.addEventListener('click', function() {
+    popupEl.classList.remove('popup_active')
+  })
+})
+
+
+
+function createElements (item) {
+  const gridElement = gridTemplate.cloneNode(true)
+  gridElement.querySelector('.elements__name').textContent = item.name
+  gridElement.querySelector('.elements__image').src = item.link
+  gridElement.querySelector('.elements__like').addEventListener('click', function (event) {
+    event.target.classList.toggle('elements__like_active')
+  })
+  const deleteButton = gridElement.querySelector('.elements__trash')
+  deleteButton.addEventListener('click', function() {
+    const gridItem = deleteButton.closest('.elements__element');
+    gridItem.remove();
+  })
+  gridElement.addEventListener('click', function () {
+  popupImageView.classList.add('popup_active')
+  popupImage.src = item.link
+  popupImageTitle.textContent = item.name
+  })
+  return gridElement
+}
+
+initialCards.forEach(function(item) {
+  const element = createElements(item)
+  gridElements.append(element)
+})
+
+const addElement = function (e) {
+  e.preventDefault()
+  const popupInputName = document.querySelector('.popup__input_type_card-name')
+  const popupDescription = document.querySelector('.popup__input_type_card-description')
+  const newElement = {
+    name: popupInputName.value,
+    link: popupDescription.value
+  }
+  const element = createElements(newElement)
+  gridElements.prepend(element)
+  popupEls.forEach (function (popupEl){
+    popupEl.classList.remove('popup_active')
+  })
+
+}
+
+formElementAdd.addEventListener('submit', addElement)
+
+formElementEdit.addEventListener('submit', formSubmitHandler)
+
+
+function openPopupEdit() {
+  popupEdit.classList.add('popup_active')
   popupInputName.value = profileTitle.textContent
   popupDescription.value = profileSubtitle.textContent
 }
 
-function popupClosed() {
-  popup.classList.remove('popup_active')
+profileEdit.addEventListener('click', openPopupEdit)
+
+function openPopupAdd() {
+  popupAdd.classList.add('popup_active')
 }
 
+addImage.addEventListener('click', openPopupAdd)
 
 function formSubmitHandler (evt) {
   evt.preventDefault()
   profileTitle.textContent = popupInputName.value
   profileSubtitle.textContent = popupDescription.value
-  popupClosed()
+  popupEls.forEach (function (popupEl){
+    popupEl.classList.remove('popup_active')
+  })
 }
 
 
 
-profileEdit.addEventListener('click', popupOpened)
 
-popupClose.addEventListener('click', popupClosed)
 
-formElement.addEventListener('submit', formSubmitHandler)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
